6b71fd393ef38fd5199f4dd769d2a3100c375f4b
StevenReale <smreale78@gmail.com>
2024-12-28 10:56:46 -0500

    First day

---
+++ AdventOfCode2024/src/Day1.java
@@ -0,0 +1,45 @@
+import java.util.*;
+
+public class Day1 {
+    public static void Part1(List<String> input) {
+        int totalDistances = 0;
+        List<Integer>[] results = parseInput(input);
+        Collections.sort(results[0]);
+        Collections.sort(results[1]);
+        for(int i = 0; i < results[0].size(); i++) {
+            totalDistances += Math.abs(results[0].get(i) - results[1].get(i));
+        }
+        System.out.print("Total Distance: ");
+        System.out.println(totalDistances);
+    }
+
+    public static void Part2(List<String> input) {
+        int similarityScore = 0;
+        List<Integer>[] parsedInput = parseInput(input);
+        Map<Integer, Integer> results = new HashMap<>();
+        for(int columnA: parsedInput[0]){
+            results.put(columnA, 0);
+        }
+        for(int columnB: parsedInput[1]){
+            if(results.containsKey(columnB)){
+                results.put(columnB, results.get(columnB) + 1);
+            }
+        }
+        for(int columnA: parsedInput[0]){
+            similarityScore += columnA * results.get(columnA);
+        }
+        System.out.print("Total Similarity Score: ");
+        System.out.println(similarityScore);
+    }
+    private static List<Integer>[] parseInput(List<String> input){
+        List<Integer>[] output = new List[2];
+        output[0] = new ArrayList<>();
+        output[1] = new ArrayList<>();
+        for(String line: input) {
+            String[] currentLine = line.split("   ");
+            output[0].add(Integer.parseInt(currentLine[0].trim()));
+            output[1].add(Integer.parseInt(currentLine[1].trim()));
+        }
+        return output;
+    }
+}
---
+++ AdventOfCode2024/src/Main.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        String file = "Library/Day1.txt";
+        List<String> input = getInput(file);
+        Day1.Part2(input);
+    }
+
+    public static List<String> getInput(String file) {
+        try(BufferedReader br = new BufferedReader(new FileReader(file))) {
+            List<String> input = new ArrayList<>();
+            String line = br.readLine();
+
+            while (line != null) {
+                input.add(line);
+                line = br.readLine();
+            }
+            return input;
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
